9525bdff49a5493e0cb190cb8ecbaf11cb300ff4
Eber Oré <eberore@inelinc.com>
2024-02-19 11:53:44 -0500

    se trabajó los códigos promocionales

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,11 @@
 import uuid
+import decimal
 
 from django.db import models
 
 from users.models import User
 from carts.models import Cart
+from promo_codes.models import PromoCode
 from shipping_addresses.models import ShippingAddress
 
 from .common import OrderStatus
@@ -22,8 +24,18 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     shipping_address = models.ForeignKey(ShippingAddress, null=True, blank=True, on_delete=models.CASCADE)
 
+    promo_code = models.OneToOneField(PromoCode, null=True, blank=True, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.order_id
+
+    def apply_promo_code(self, promo_code):
+        if self.promo_code is None:
+            self.promo_code = promo_code
+            self.save()
+
+            self.update_total()
+            promo_code.use()
 
     def get_or_set_shipping_address(self):
         if self.shipping_address:
@@ -51,8 +63,14 @@ class Order(models.Model):
         self.total = self.get_total()
         self.save()
 
+    def get_discount(self):
+        if self.promo_code:
+            return self.promo_code.discount
+        
+        return 0
+
     def get_total(self):
-        return self.cart.total + self.shipping_total
+        return self.cart.total + self.shipping_total - decimal.Decimal(self.get_discount())
 
 def set_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
